d4730efcdca211ab7d8f581d7c38bd730d2e951d
eshangin <shangin.jekon@mail.ru>
2019-02-06 16:29:46 +0700

    view requested data on map

---
+++ Example.CaptureMapMarkerMovements/Models/Waypoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.CaptureMapMarkerMovements.Models
+{
+    public class Waypoint
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+        public decimal Speed { get; set; }
+        public string Date { get; set; }
+    }
+}
