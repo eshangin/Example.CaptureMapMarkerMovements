c180e4f1036eb87756091188d26f348d2dfddeff
eshangin <shangin.jekon@mail.ru>
2019-02-06 14:49:23 +0700

    add User-Agent for phantom requests which seems helps resolve Map Load issues

--- Example.CaptureMapMarkerMovements/Scripts/phantomjs-capture.js
+++ Example.CaptureMapMarkerMovements/Scripts/phantomjs-capture.js
@@ -9,6 +9,7 @@ if (!outputPath) {
 }
 
 var page = require("webpage").create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
 var timeoutInSeconds = 120;
 var width = 1100;
