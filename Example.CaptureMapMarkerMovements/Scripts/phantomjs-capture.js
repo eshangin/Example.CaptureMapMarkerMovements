6a4a1befb8df5813d84a855e60de9ae2b78e6e62
eshangin <shangin.jekon@mail.ru>
2019-02-07 12:09:03 +0700

    cleanup

--- Example.CaptureMapMarkerMovements/Scripts/phantomjs-capture.js
+++ Example.CaptureMapMarkerMovements/Scripts/phantomjs-capture.js
@@ -114,11 +114,7 @@ function pad(num, size) {
 }
 
 function formatDate(m) {
-    return m.getUTCFullYear() + "/" +
-        ("0" + (m.getUTCMonth() + 1)).slice(-2) + "/" +
-        ("0" + m.getUTCDate()).slice(-2) + " " +
-        ("0" + m.getUTCHours()).slice(-2) + ":" +
-        ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+    return ("0" + m.getUTCMinutes()).slice(-2) + ":" +
         ("0" + m.getUTCSeconds()).slice(-2) + ":" +
         m.getUTCMilliseconds();
 }
