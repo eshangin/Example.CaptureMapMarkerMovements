ff69dfe5afcc56c1913340d9658351eb196dc9bd
eshangin <shangin.jekon@mail.ru>
2019-02-08 15:02:17 +0700

    cleanup

--- Example.CaptureMapMarkerMovements/App_Start/BundleConfig.cs
+++ Example.CaptureMapMarkerMovements/App_Start/BundleConfig.cs
@@ -8,11 +8,7 @@ namespace Example.CaptureMapMarkerMovements
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
